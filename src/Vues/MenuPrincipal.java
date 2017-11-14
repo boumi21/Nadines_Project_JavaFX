31d3a9ad51ab585af1c252d9a71b27e35bb6e491
boumi21 <glorkpatate@yahoo.be>
2017-11-13 23:08:15 -0500

    Construction MenuPrincipal #1
    Première partie de la création du menu principal.

---
+++ src/Vues/MenuPrincipal.java
@@ -0,0 +1,43 @@
+package Vues;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+import java.sql.SQLException;
+
+public class MenuPrincipal extends Region {
+
+    public MenuPrincipal(){
+        super();
+        ConstruirePanneau();
+    }
+
+    private void ConstruirePanneau(){
+
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10));
+        vBox.setPrefSize(400, (600-30));
+
+        Button btnActionProximite = new Button("Proximite en fonction du temps");
+        Button btnActionTemperature = new Button("Temperature en fonction du temps");
+        Button btnActionProximiteEtTemperature = new Button("Proximite en fonction de la temperature");
+
+        btnActionProximite.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                try {
+                    ControleurVue.getInstance().actionAjouterItem();
+                } catch (ClassNotFoundException | SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
