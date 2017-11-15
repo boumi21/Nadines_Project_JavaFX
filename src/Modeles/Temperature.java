89775c29db3465bfcc67bf24134ea6a93f80b110
boumi21 <glorkpatate@yahoo.be>
2017-11-15 16:06:04 -0500

    Creation modele Temperature
    Création du modèle pour la température.

---
+++ src/Modeles/Temperature.java
@@ -0,0 +1,31 @@
+package Modeles;
+
+public class Temperature {
+    protected float degres;
+    protected String date;
+
+    public Temperature(){
+
+    }
+
+    public Temperature(float degres, String date){
+        this.degres = degres;
+        this.date = date;
+    }
+
+    public float getDegres() {
+        return degres;
+    }
+
+    public void setDegres(float degres) {
+        this.degres = degres;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
