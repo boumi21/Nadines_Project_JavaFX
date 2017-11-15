36e70195ce2f79eb6c2570ce0abbbf114c300849
boumi21 <glorkpatate@yahoo.be>
2017-11-15 16:07:13 -0500

    Creation modele proximite
    Création du modèle pour la proximité.

---
+++ src/Modeles/Proximite.java
@@ -0,0 +1,32 @@
+package Modeles;
+
+public class Proximite {
+
+    protected float valeurProximite;
+    protected String date;
+
+    public Proximite(){
+
+    }
+
+    public Proximite(float valeurProximite, String date) {
+        this.valeurProximite = valeurProximite;
+        this.date = date;
+    }
+
+    public float getValeurProximite() {
+        return valeurProximite;
+    }
+
+    public void setValeurProximite(float valeurProximite) {
+        this.valeurProximite = valeurProximite;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
