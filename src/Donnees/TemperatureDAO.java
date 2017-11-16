0926fc838b1ddaabcdf2ed432bbbdd81ab88ec4c
boumi21 <glorkpatate@yahoo.be>
2017-11-16 15:00:33 -0500

    TemperatureDAO #2
    Création de la méthode CreerListeTemperature qui servira à construire la liste des températures qui sera ensuite utilisée pur les graphiques.

--- src/Donnees/TemperatureDAO.java
+++ src/Donnees/TemperatureDAO.java
@@ -1,11 +1,26 @@
 package Donnees;
 
+import Modeles.Temperature;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TemperatureDAO {
+    private List<Temperature> listeTemperature;
 
     private static TemperatureDAO instance;
 
     public static TemperatureDAO getInstance(){
         if (instance == null) instance = new TemperatureDAO();
         return instance;
+    }
+
+    public List<Temperature> CreerListeTemperature(Temperature temperature){
+        if (listeTemperature == null){
+            listeTemperature = new ArrayList<Temperature>();
+        }
+
+        listeTemperature.add(temperature);
+        return listeTemperature;
     }
 }
